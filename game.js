d4af04b62edf80a603a89fb0f11ed29e484868ca
alankylam <31635981+alankylam@users.noreply.github.com>
2020-07-30 17:05:37 -0700

    Initial Commit

---
+++ game.js
@@ -0,0 +1,91 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var level = 0;
+var started = false;
+
+$(document).keypress(function(){
+if(started === false) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+}
+})
+
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    } else{
+        playSound("wrong");
+        $("body").addClass("game-over")
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+    }
+}
+
+
+function nextSequence(){
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+
+}
+
+$(".btn").click(function(){
+    var userChosenColor =$(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
+})
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+
+function startOver(){
+    level =0;
+    gamePattern = [];
+    started = false;
+}
+
+// function playNext(){
+//     for (var i=0; i <gamePattern.length; i++){
+//         setTimeout(function () {
+//             playSound(gamePattern[i]);
+//         }, 5000);
+//     }}
